9b2db75c41269d14013eea812285f2eed394d05b
Furquan Uddin <furquanuddin94@gmail.com>
2024-08-12 11:52:35 +0700

    testing again

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ExchangeRatesList from '../components/ExchangeRatesList'; // Import the client component
 import { ExchangeRateInfo } from './common/model/ExchangeRateInfo';
+import { cookies, headers } from 'next/headers';
 
 export const fetchCache = 'force-no-store'
 
@@ -11,8 +12,17 @@ const fetchExchangeRates = async () => {
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
     : 'http://localhost:3000';
 
+  console.log("Hostname", hostname);
+
   try {
     console.log("Fetching exchange rates from next apis");
+
+    // Get the cookies
+    const cookieStore = cookies();
+    const cookie = cookieStore.get('_vercel_jwt');
+
+    console.log("cookie", cookie?.name, cookie?.value);
+
 
     const [latestRateResponse, latestDeeMoneyRateResponse, latestWesternUnionRateResponse] = await Promise.all([
       fetch(hostname + '/api/getLatestRates', { credentials: "same-origin" }),
