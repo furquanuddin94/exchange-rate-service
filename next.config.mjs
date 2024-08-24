85184046ea58bbf57296fdab6872896e914d8d20
Furquan Uddin <furquanuddin94@gmail.com>
2024-08-24 17:32:10 +0700

    revert change

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        env: process.env.NEXT_PUBLIC_VERCEL_ENV
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
