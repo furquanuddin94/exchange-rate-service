488f4553e5088862d0c7be14d7a46200697c0659
Furquan Uddin <furquanuddin94@gmail.com>
2024-08-24 20:43:03 +0700

    feat: Chart dropdown (#6)
    * feat: adding dropdown in chart
    
    * fix
    
    * filtering data for time range
    
    * test change
    
    * fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import { ModeToggle } from '@/components/mode-toggle';
 import { MultiLineChart } from '@/components/multi-line-chart';
-// import { cookies, headers } from 'next/headers';
 import React from 'react';
-import FxRateCards from '../components/fx-rate-cards'; // Import the client component
+import FxRateCards from '../components/fx-rate-cards';
 import { constants } from './utils/envUtils';
 export const dynamic = 'force-dynamic';
 
